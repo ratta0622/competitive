d684cfbb80c193377e4218ba800949d986b8b960
ratta0622 <ratta0622@gmail.com>
2020-12-16 20:01:01 +0900

    ARC096/D/D_fast.cpp is not finished

--- drken/ARC096/D-StaticSushi/D_fast.cpp
+++ drken/ARC096/D-StaticSushi/D_fast.cpp
@@ -10,29 +10,58 @@ int main(){
   ll x[N+2], v[N+2];
   x[0] = 0; v[0] = 0;
   x[N+1] = C; v[N+1] = 0;
+
+
   for(int i=1; i<=N; ++i){
     cin >> x[i] >> v[i];
   }
 
-  ll max = 0;
+  ll f_a[N+2];
+  ll f_b[N+2];
 
-  ll sum_v=0;
-  for(int i=0; i<=N; ++i){
-    for(int j=N+1; j>i; --j){
-      sum_v=0;
-      for(int k=0; k<=i; ++k){
-        sum_v += v[k];
-      }
-      for(int k=N+1; k>=j; --k){
-        sum_v += v[k];
-      }
-      if(sum_v-2*x[i]-(C-x[j]) > max) max = sum_v-2*x[i]-(C-x[j]);
-      if(sum_v-2*(C-x[j])-x[i] > max) max = sum_v-2*(C-x[j])-x[i];
-    }
+  f_a[0] = 0;
+  f_a[N+1] = 0;
+  for(int i=1; i<=N; ++i){
+    f_a[i] = f_a[i-1] + v[i] + x[i-1] - x[i];
   }
 
-  cout << max << endl;
+  f_b[N+1] = 0;
+  f_b[0] = 0;
+  for(int i=N; i>=1; --i){
+    f_b[i] = f_b[i+1] + v[i] + (C-x[i+1]) - (C-x[i]);
+  }
+
+  ll max_f_a[N+1];
+  ll max_f_b[N+1];
+  max_f_a[0] = f_a[0];
+  max_f_b[0] = f_b[0];
+  for(int i=1; i<=N; ++i){
+    max_f_a[i] = max(max_f_a[i-1], f_a[i]);
+  }
+  for(int i=N; i>=1; --i){
+    max_f_b[i] = max(max_f_b[i+1], f_b[i]);
+  }
+
+  ll max_v = 0;
+  ll sum_v = 0;
+
+  /* A which is turning point clockwise is fixed */
+  for(int i=0; i<=N; ++i){
+    max_v = max(max_v, max_f_b[i+1]+f_a[i]-x[i]);
+  }
+
+  /* B which is turning point clockwise is fixed */
+  for(int i=N+1; i>=1; --i){
+    max_v = max(max_v, max_f_a[i-1]+f_b[i]-(C-x[i]));
+  }
+
+  cout << max_v << endl;
   
+
+  for(int i=0; i<N+2; ++i){
+    cout << f_a[i] << " " << f_b[i] << " " <<  max_f_a[i] << " " << max_f_b[i] << endl;
+  }
+
 
   return 0;
 }
