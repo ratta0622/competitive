41ec9927316d9d8de41dfee665decd346acc3283
ratta0622 <ratta0622@gmail.com>
2020-12-22 00:06:24 +0900

    ABC057 D

--- drken/ABC057/D-MaximumAverageSets/D.cpp
+++ drken/ABC057/D-MaximumAverageSets/D.cpp
@@ -12,66 +12,54 @@ int main(){
 
   sort(v, v+N, greater<ll>());
 
-  ll sum=0;
-  double mean[B-A+1]={};
-
-  for(int i=0; i<B; ++i){
-    sum += v[i];
-    if(i>=A-1){
-      mean[i-A+1] = (double)sum/( i+1 );
-      if(mean[i-A+1] != mean[0]) break;
-    }
-  }
-
-  /* for(auto i=0; i<B-A+1; ++i){ */
-  /*   cout << "mean[" << i << "]:" << mean[i] << " "; */
-  /* } */
-  /* cout << endl; */
-
-  int m   = 0;
-  int n   = 0;
+  ll sum=0LL;
+  double mean;
 
   for(int i=0; i<A; ++i){
-    if(v[i]==v[A-1]) ++m;    
+    sum += v[i];
   }
-  for(int i=A; i<N; ++i){
-    if(v[i]==v[A-1]) ++n;
+  mean = (double)sum/A;
+
+  int n = 0;
+  for(int i=0; i<N; ++i){
+      if(v[i]==v[A-1]) ++n;
   }
 
   /* for(int i=0; i<N; ++i){ */
   /*   cout << v[i] << " "; */
   /* } */
   /* cout << endl; */
+  /* cout << "n:" << n << endl; */
 
-  /* cout << "m:" << m << " n:" << n << endl; */
-
-  ll comb[B-A+1]={};
+  /* comb[i] is nCi */
+  ll comb[N+1]={};
   comb[0]=1;
-  for(int i=0; i<m; ++i){
-    comb[0] *= (m+n-i);
-    comb[0] /= (m-i);
-  }
-
-  int count=0;
-  for(int i=1; i<B-A+1; ++i){
-    if(mean[i] != mean[0]) break;
-    ++ count;
-  }
-
-  for(int i=1; i<=count; ++i){
+  for(int i=1; i<=N; ++i){
     if(i>n) break;
-    comb[i] = comb[i-1];
-    comb[i] *= (n-i+1);
-    comb[i] /= (m+i);
+    comb[i] = comb[i-1]*(n-i+1)/i;
   }
 
-  /* for(int i=0; i<B-A+1; ++i){ */
+
+  /* for(int i=0; i<N; ++i){ */
   /*   cout << "comb[" << i <<"]:" << comb[i] << endl; */
   /* } */
 
-  cout << mean[0] << endl;
-  cout << accumulate(comb, comb+(B-A+1), 0LL) << endl;
-  
+  int m = 0;
+  for(int i=0; i<A; ++i){
+      if(v[i]==v[A-1]) ++m;
+  }
+
+  ll answer = 0LL;
+  if(v[0]!=v[A-1]){
+    answer = comb[m];
+  }else{
+    for(int i=A;i<=B;++i){
+      answer += comb[i];
+    }
+  }
+
+  cout << mean << endl;
+  cout << answer << endl;
 
   return 0;
 }
