c092f2a7b05be415ff880357fda19f9d65b9fc84
ratta0622 <ratta0622@gmail.com>
2021-01-18 02:33:02 +0900

    ABC103-D has not been solved

---
+++ ants/beginner_ants_AtCoder/2-2_greedy/2-2-2/ABC103-D/D.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int N, M;
+  cin >> N >> M;
+  int a[M], b[M];
+  for(int i=0; i<M; ++i){
+    cin >> a[i] >> b[i];
+  }
+  
+
+  return 0;
+}
--- ants/beginner_ants_AtCoder/2-2_greedy/2-2-2/keyence2020/B.cpp
+++ ants/beginner_ants_AtCoder/2-2_greedy/2-2-2/keyence2020/B.cpp
@@ -1,34 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+using P = pair<ll,ll>;
 
 int main(){
   int N;
   cin >> N;
-  map<ll,ll> robot;
+  vector<P> robot(N);
   for(int i=0; i<N; ++i){
     ll X, L;
     cin >> X >> L;
-    robot[X+L] = X-L;
+    robot[i] = {X+L, X-L};
   }
 
-  for(auto i=robot.begin(); i!=prev(robot.end(),1); ++i){
-    if(i == robot.end()) break;
-    for(auto j=next(i,1); j!=robot.end(); ++j){
-      if(i->first > j->second){
-        robot.erase(j);
-        --j;
-      }
-    }
+  sort(robot.begin(), robot.end());
+
+  /* for(auto i=robot.begin(); i!=prev(robot.end(),1); ++i){ */
+  /*   if(i == robot.end()) break; */
+  /*   for(auto j=next(i,1); j!=robot.end(); ++j){ */
+  /*     if(i->first > j->second){ */
+  /*       robot.erase(j); */
+  /*       --j; */
+  /*     } */
+  /*   } */
+  /* } */
+  
+  int ans = 1;
+  ll comp = robot[0].first;
+  for(auto i=next(robot.begin(),1); i!=robot.end(); ++i){
+    if(comp > i->second) continue;
+    ++ans;
+    comp = i->first;
   }
 
-  cout << endl;
-  for(auto i=robot.begin(); i!=robot.end(); ++i){
-    cout << i->second << " " << i->first << endl;
-  }
-  cout << endl;
+  /* cout << endl; */
+  /* for(auto i=robot.begin(); i!=robot.end(); ++i){ */
+  /*   cout << i->second << " " << i->first << endl; */
+  /* } */
+  /* cout << endl; */
 
-  cout << robot.size() << endl;
+  cout << ans << endl;
 
   return 0;
 }
